1cb8a0fa441e3f24d3fad9d8244cdcaf25ca75ef
Rolando Fernandez <rfernandez@pros.com>
2026-01-15 21:13:09 +0100

    responsive pricing table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
-import './App.scss'
+import './App.css'
 
 function App() {
   return (
     <div className="app">
-      <h1>React App Template</h1>
-      <p>Start building your application.</p>
+      <div className="pricing">
+        <div className="card basic">
+          <div className="card__title">Basic</div>
+          <div className="card__price">9<sup>$</sup></div>
+          <div className="card__features">
+            <ul className="features__list">
+              <li>Feature 1</li>
+              <li>Feature 1</li>
+              <li>Feature 1</li>
+              <li className="feature--notIncluded">Feature 1</li>
+              <li className="feature--notIncluded">Feature 1</li>
+            </ul>
+          </div>
+          <div className="card__cta">
+            <button>Buy now </button>
+          </div>
+        </div>
+        <div className="card pro">
+          <div className="card__title">Pro</div>
+          <div className="card__price">29<sup>$</sup></div>
+          <div className="card__features">
+            <ul className="features__list">
+              <li>Feature 1</li>
+              <li>Feature 1</li>
+              <li>Feature 1</li>
+              <li>Feature 1</li>
+              <li className="feature--notIncluded">Feature 1</li>
+            </ul>
+          </div>
+          <div className="card__cta">
+            <button>Buy now </button>
+          </div>
+        </div>
+        <div className="card enterprise">
+          <div className="card__title">Enterprise</div>
+          <div className="card__price">99<sup>$</sup></div>
+          <div className="card__features">
+            <ul className="features__list">
+              <li>Feature 1</li>
+              <li>Feature 1</li>
+              <li>Feature 1</li>
+              <li>Feature 1</li>
+              <li>Feature 1</li>
+            </ul>
+          </div>
+          <div className="card__cta">
+            <button>Buy now </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
